b4b802a079f7a2b60b70f7f8d3b8f91cc4f7e82d
OuHua Chang <tony12200126@gmail.com>
2025-03-06 19:38:16 +0800

    Add initial implementation of queue.c
    Added and tested the following functions in queue.c:
    - q_new()
    - q_free()
    - q_insert_head()/q_insert_tail()
    - q_remove_head()/q_remove_tail()
    - q_size()
    - q_delete_mid()
    - q_delete_dup()
    
    All functions have been tested and confirmed to work correctly.
    
    Change-Id: Ic941cc6230f90836105b76a0bf37a25849987fd7

--- queue.c
+++ queue.c
@@ -7,46 +7,127 @@
 /* Create an empty queue */
 struct list_head *q_new()
 {
-    return NULL;
+    struct list_head *head =
+        (struct list_head *) malloc(sizeof(struct list_head));
+    if (head)
+        INIT_LIST_HEAD(head);
+    return head;
 }
 
 /* Free all storage used by queue */
-void q_free(struct list_head *head) {}
+void q_free(struct list_head *head)
+{
+    if (!head)
+        return;
+    element_t *del, *safe = NULL;
+
+    list_for_each_entry_safe (del, safe, head, list) {
+        if (del->value)
+            free(del->value);
+        free(del);
+    }
+    free(head);
+}
 
 /* Insert an element at head of queue */
 bool q_insert_head(struct list_head *head, char *s)
 {
+    element_t *p = (element_t *) malloc(sizeof(element_t));
+    if (!head || !p)
+        return false;
+
+    p->value = strdup(s);
+    if (!p->value) {
+        free(p);
+        return false;
+    }
+
+    list_add(&p->list, head);
     return true;
 }
 
 /* Insert an element at tail of queue */
 bool q_insert_tail(struct list_head *head, char *s)
 {
+    element_t *p = (element_t *) malloc(sizeof(element_t));
+    if (!head || !p)
+        return false;
+
+    p->value = strdup(s);
+    if (!p->value) {
+        free(p);
+        return false;
+    }
+
+    list_add_tail(&p->list, head);
     return true;
 }
 
 /* Remove an element from head of queue */
 element_t *q_remove_head(struct list_head *head, char *sp, size_t bufsize)
 {
-    return NULL;
+    if ((!head) || list_empty(head))
+        return NULL;
+
+    element_t *p = list_first_entry(head, element_t, list);
+    (p->list).prev->next = (p->list).next;
+    (p->list).next->prev = (p->list).prev;
+
+    if (sp) {
+        strncpy(sp, p->value, bufsize - 1);
+        sp[bufsize - 1] = '\0';
+    }
+    return p;
 }
 
 /* Remove an element from tail of queue */
 element_t *q_remove_tail(struct list_head *head, char *sp, size_t bufsize)
 {
-    return NULL;
+    if ((!head) || list_empty(head))
+        return NULL;
+
+    element_t *p = list_last_entry(head, element_t, list);
+    (p->list).prev->next = (p->list).next;
+    (p->list).next->prev = (p->list).prev;
+
+    if (sp) {
+        strncpy(sp, p->value, bufsize - 1);
+        sp[bufsize - 1] = '\0';
+    }
+    return p;
 }
 
 /* Return number of elements in queue */
 int q_size(struct list_head *head)
 {
-    return -1;
+    if (!head)
+        return 0;
+
+    int len = 0;
+    struct list_head *li;
+
+    list_for_each (li, head)
+        len++;
+    return len;
 }
 
 /* Delete the middle node in queue */
 bool q_delete_mid(struct list_head *head)
 {
     // https://leetcode.com/problems/delete-the-middle-node-of-a-linked-list/
+    if ((!head) || list_empty(head))
+        return false;
+
+    struct list_head *fast, *slow;
+    for (fast = head->next->next, slow = head->next;
+         fast != head && fast != head->prev;
+         fast = fast->next->next, slow = slow->next) {
+    }
+
+    element_t *e = list_entry(slow, element_t, list);
+    (e->list).prev->next = (e->list).next;
+    (e->list).next->prev = (e->list).prev;
+    q_release_element(e);
     return true;
 }
 
@@ -54,6 +135,32 @@ bool q_delete_mid(struct list_head *head)
 bool q_delete_dup(struct list_head *head)
 {
     // https://leetcode.com/problems/remove-duplicates-from-sorted-list-ii/
+    if (!head || list_empty(head))
+        return false;
+
+    struct list_head *curr = head->next;
+    while (curr != head && curr->next != head) {
+        element_t *e = list_entry(curr, element_t, list);
+        const element_t *e_next = list_entry(curr->next, element_t, list);
+        /*  If found duplicate nodes, store the duplicate word to s,
+         *  then delete duplicate nodes until found different words.
+         */
+        if (strcmp(e->value, e_next->value) == 0) {
+            struct list_head *prev = curr->prev;
+            char *s = strdup(e->value);
+            while (strcmp(e->value, s) == 0) {
+                curr = curr->next;
+                q_release_element(e);
+                if (curr == head)
+                    break;
+                e = list_entry(curr, element_t, list);
+            }
+            free(s);
+            prev->next = curr;
+            curr->prev = prev;
+        } else
+            curr = curr->next;
+    }
     return true;
 }
 
