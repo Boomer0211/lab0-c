05b8687f3cbb88532f9563f63ad867479c12c6d6
OuHua Chang <tony12200126@gmail.com>
2025-03-15 02:04:42 +0800

    Clarify comparison logic in q_ascend and q_descend
    Refine the string comparison logic in q_ascend() and q_descend() for
    better readability and correctness. This change ensures that the
    conditions for maintaining elements in ascending and descending order
    are more intuitive and consistent.
    
    Change-Id: I637b6c7c5858b7095271a55f77ca8e6e4ac88c43

--- queue.c
+++ queue.c
@@ -275,7 +275,7 @@ int q_ascend(struct list_head *head)
 
     list_for_each_safe(li, tmp, head) {
         element_t *e = list_entry(li, element_t, list);
-        if (!min || strcmp(e->value, min) < 0)
+        if (!min || strcmp(min, e->value) >= 0)
             min = e->value;
         else {
             list_del(li);
@@ -302,7 +302,7 @@ int q_descend(struct list_head *head)
 
     list_for_each_safe(li, tmp, head) {
         element_t *e = list_entry(li, element_t, list);
-        if (!max || strcmp(e->value, max) > 0)
+        if (!max || strcmp(max, e->value) <= 0)
             max = e->value;
         else {
             list_del(li);
